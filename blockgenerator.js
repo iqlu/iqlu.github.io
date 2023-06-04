3a454b0e9b32ed024afbee1266e70385e173d85c
iqlu <135066095+iqlu@users.noreply.github.com>
2023-06-04 15:10:59 +0300

    Add files via upload

---
+++ blockgenerator.js
@@ -0,0 +1,175 @@
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const blockimage = document.getElementById('image')
+const button = document.querySelector('button');
+button.addEventListener('click', function() {rollnumber()});
+
+function rollnumber(){
+  const folder = getRandomInt(2, 9);
+  let page = 0;
+  let block = 0;
+  if (folder === 2) {
+    page = getRandomInt(1, 4);
+    if (page === 1) {
+      block = getRandomInt(1, 7);
+    }
+    if (page === 2) {
+      block = getRandomInt(1, 8);
+    }
+    if (page === 3) {
+      block = getRandomInt(1, 9);
+    }
+    if (page === 4) {
+      block = getRandomInt(1, 8);
+    }
+  }
+
+  if (folder === 3) {
+    page = getRandomInt(1, 4);
+    if (page === 1) {
+      block = getRandomInt(1, 7);
+    }
+    if (page === 2) {
+      block = getRandomInt(1, 6);
+    }
+    if (page === 3) {
+      block = getRandomInt(1, 4);
+    }
+    if (page === 4) {
+      block = getRandomInt(1, 8);
+    }
+  }
+
+  if (folder === 4) {
+    page = getRandomInt(1, 5);
+    if (page === 1) {
+      block = getRandomInt(1, 9);
+    }
+    if (page === 2) {
+      block = getRandomInt(1, 6);
+    }
+    if (page === 3) {
+      block = getRandomInt(1, 8);
+    }
+    if (page === 4) {
+      block = getRandomInt(1, 8);
+    }
+    if (page === 5) {
+      block = getRandomInt(1, 4);
+    }
+  }
+
+  if (folder === 5) {
+    page = getRandomInt(1, 5);
+    if (page === 1) {
+      block = getRandomInt(1, 7);
+    }
+    if (page === 2) {
+      block = getRandomInt(1, 9);
+    }
+    if (page === 3) {
+      block = getRandomInt(1, 7);
+    }
+    if (page === 4) {
+      block = getRandomInt(1, 6);
+    }
+    if (page === 5) {
+      block = getRandomInt(1, 4);
+    }
+  }
+
+  if (folder === 6) {
+    page = getRandomInt(1, 9);
+    if (page === 1) {
+      block = getRandomInt(1, 5);
+    }
+    if (page === 2) {
+      block = getRandomInt(1, 9);
+    }
+    if (page === 3) {
+      block = getRandomInt(1, 6);
+    }
+    if (page === 4) {
+      block = getRandomInt(1, 8);
+    }
+    if (page === 5) {
+      block = getRandomInt(1, 8);
+    }
+    if (page === 6) {
+      block = getRandomInt(1, 8);
+    }
+    if (page === 7) {
+      block = getRandomInt(1, 8);
+    }
+    if (page === 8) {
+        block = getRandomInt(1, 4);
+    }
+    if (page === 9) {
+        block = getRandomInt(1, 9);
+    }
+  }
+
+  if (folder === 7) {
+    page = getRandomInt(1, 6);
+    if (page === 1) {
+      block = getRandomInt(1, 9);
+    }
+    if (page === 2) {
+      block = getRandomInt(1, 5);
+    }
+    if (page === 3) {
+      block = getRandomInt(1, 6);
+    }
+    if (page === 4) {
+      block = getRandomInt(1, 5);
+    }
+    if (page === 5) {
+      block = getRandomInt(1, 6);
+    }
+    if (page === 6) {
+      block = getRandomInt(1, 7);
+    }
+  }
+
+  if (folder === 8) {
+    page = getRandomInt(1, 9);
+    if (page === 1) {
+      block = getRandomInt(1, 4);
+    }
+    if (page === 2) {
+      block = getRandomInt(1, 7);
+    }
+    if (page === 3) {
+      block = getRandomInt(1, 8);
+    }
+    if (page === 4) {
+      block = getRandomInt(1, 6);
+    }
+    if (page === 5) {
+      block = getRandomInt(1, 4);
+    }
+    if (page === 6) {
+      block = getRandomInt(1, 8);
+    }
+    if (page === 7) {
+      block = getRandomInt(1, 6);
+    }
+    if (page === 8) {
+        block = getRandomInt(1, 7);
+    }
+    if (page === 9) {
+        block = getRandomInt(1, 7);
+    }
+  }
+  if (folder === 9) {
+    page = getRandomInt(1, 4);
+  }
+let result = (folder * 100) + (page * 10) + block;
+document.getElementById("block").innerHTML = folder + '-' + page + '-' + block;
+blockimage.setAttribute("src", "img/pic"+result+".jpg");
+}
+
+
